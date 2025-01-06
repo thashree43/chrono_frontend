97d2b689f313fc1025172d4655235c7f454f08fe
Thashreef <thashreefkhan4@gmail.com>
2025-01-06 15:34:01 +0530

    newe one

--- src/section/Addproduct.jsx
+++ src/section/Addproduct.jsx
@@ -126,7 +126,7 @@ export const AddProductModal = ({
               required
             />
           </div>
-          {/* <div>
+          <div>
             <Typography variant="small" color="blue-gray" className="mb-2">
               Status
             </Typography>
@@ -140,7 +140,7 @@ export const AddProductModal = ({
               <option value="Low Stock">Low Stock</option>
               <option value="Limited">Limited</option>
             </select>
-          </div> */}
+          </div>
           <div className="flex justify-end space-x-4">
             <Button variant="text" color="blue-gray" onClick={onClose}>
               Cancel
